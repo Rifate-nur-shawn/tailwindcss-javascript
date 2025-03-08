1b229af5a8e144bfc1a3ae7a2649fa7177ce86a2
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-03-08 17:26:38 +0600

    2nd commit

--- node.js
+++ node.js
@@ -129,7 +129,19 @@ class dog extends animal{
     }
 }
 
-const cat = new animal('CAT', 10);
-const dod =new dog('DOG', 20, 'BULLDOG');
-console.log(cat.name, cat.age);
-console.log(dod.name, dod.age, dod.breed);
+// const cat = new animal('CAT', 10);
+// const dod =new dog('DOG', 20, 'BULLDOG');
+// console.log(cat.name, cat.age);
+// console.log(dod.name, dod.age, dod.breed);
+
+
+const nums = [1,2,3,4,5];
+let output = nums.filter(n => n%2);
+console.log(output);
+
+const cube=x=> x*x*x; 
+console.log(cube(2))
+const {x, y, z} = {x: 1, y1: 2, z: 3};
+console.log(y);
+function min(nums) { return Math.min(nums) }
+console.log(min( [1,3,2 ]));
